18a2bf206e0eb042d27116c951a392d9d83c1d77
Filip Zagozdon <filipzagozdon8@gmail.com>
2022-09-08 05:33:22 +0200

    Test main page via static html

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { loadInitialStructure } from './loadup'
+
+loadInitialStructure();
---
+++ src/loadup.js
@@ -0,0 +1,68 @@
+function createHeaderContainer() {
+    const headerContainer = document.createElement('div');
+    headerContainer.setAttribute('id', 'header');
+    headerContainer.classList.add('text-box');
+    return headerContainer;
+}
+
+function createLogo() {
+    const logo = document.createElement('h1');
+    logo.textContent = "Luigi's";
+    return logo;
+}
+
+function createNavigationWrapper() {
+    const navigationWrapper = document.createElement('div');
+    navigationWrapper.setAttribute('id', 'nav');
+    return navigationWrapper;
+}
+
+function createNavigationList() {
+    const navigationList = document.createElement('ul');
+
+    const homeButton = document.createElement('li');
+    homeButton.textContent = 'Home';
+
+    const menuButton = document.createElement('li');
+    menuButton.textContent = 'Menu';
+
+    const contactButton = document.createElement('li');
+    contactButton.textContent = 'Contact';
+
+    navigationList.appendChild(homeButton);
+    navigationList.appendChild(menuButton);
+    navigationList.appendChild(contactButton);
+    return navigationList;
+}
+
+function createNavigation() {
+    const navigationWrapper = createNavigationWrapper();
+    const navigationList = createNavigationList();
+
+    navigationWrapper.appendChild(navigationList);
+    return navigationWrapper;
+}
+
+function createContentWrapper() {
+    const contentWrapper = document.createElement('div');
+    contentWrapper.setAttribute('id', 'content')
+    return contentWrapper;
+}
+
+function createHeader() {
+    const headerWrapper = createHeaderContainer();
+    const logo = createLogo();
+    const navigation = createNavigation();
+
+    headerWrapper.appendChild(logo);
+    headerWrapper.appendChild(navigation);
+}
+
+export function loadInitialStructure() {
+    const body = document.querySelector('body');
+    const header = createHeader();
+    const contentWrapper = createContentWrapper();
+
+    body.appendChild(header);
+    body.appendChild(contentWrapper);
+}
